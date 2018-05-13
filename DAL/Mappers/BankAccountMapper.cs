120799c49be305ca30379cc08bc2e24af5ffa3eb
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-05-13 21:37:24 +0300

    Add files via upload

---
+++ DAL/Mappers/BankAccountMapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DAL.Interface.DTO;
+using ORM;
+
+namespace DAL.Mappers
+{
+    public static class BankAccountMapper
+    {
+        public static BankAccount FromDTO(this BankAccountDTO dto)
+        {
+            if (dto is null)
+            {
+                return null;
+            }
+
+            return new BankAccount
+            {
+                IsClosed = dto.IsClosed,
+                AccountNumberID = dto.AccountNumber,
+                AccountType = dto.Type.ToString(),
+                Amount = dto.Amount,
+                BonusPoints = dto.BonusPoints,
+                OwnerID = dto.Owner.Passport
+            };
+        }
+
+        public static BankAccountDTO ToDTO(this BankAccount account)
+        {
+            if (account is null)
+            {
+                return null;
+            }
+
+            return new BankAccountDTO(
+                account.IsClosed,
+                account.AccountNumberID,
+                new ClientDTO(account.Owner.Firstname, account.Owner.Lastname, account.Owner.PassportID, account.Owner.Email),
+                account.Amount,
+                account.BonusPoints,                
+                (BankAccountDTO.AccountType)Enum.Parse(typeof(BankAccountDTO.AccountType), account.AccountType));
+        }
+    }
+}
---
+++ DAL/Mappers/ClientMapper.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Interface.DTO;
+using ORM;
+
+namespace DAL.Mappers
+{
+    public static class ClientMapper
+    {
+        public static Client FromDTO(this ClientDTO dto) => new Client() { Firstname = dto.Firstname, Lastname = dto.Lastname, PassportID = dto.Passport, Email = dto.Email };
+
+        public static ClientDTO ToDTO(this Client owner) => new ClientDTO(owner.Firstname, owner.Lastname, owner.PassportID, owner.Email);
+    }
+}
