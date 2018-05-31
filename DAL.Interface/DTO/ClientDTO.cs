c179f36ec8e7154f32a20b3783c80da958e941b9
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-05-31 11:08:39 +0300

    Add files via upload

--- DAL.Interface/DTO/ClientDTO.cs
+++ DAL.Interface/DTO/ClientDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace DAL.Interface.DTO
+﻿using System.Collections.Generic;
+
+namespace DAL.Interface.DTO
 {
     public class ClientDTO
     {        
@@ -29,5 +31,60 @@
         /// Email adress of client
         /// </summary>
         public string Email { get; set; }
+
+        /// <summary>
+        /// Defining operator '==' for comparing two Client instances
+        /// </summary>
+        /// <param name="clientA">1st Client instance</param>
+        /// <param name="clientB">2nd Client instance</param>
+        /// <returns>True if instances are equal; otherwise, false</returns>
+        public static bool operator ==(ClientDTO clientA, ClientDTO clientB) => clientA.Firstname == clientB.Firstname && clientA.Lastname == clientB.Lastname && clientA.Passport == clientB.Passport;
+
+        /// <summary>
+        /// Defining operator '!=' for comparing two Client instances
+        /// </summary>
+        /// <param name="clientA">1st Client instance</param>
+        /// <param name="clientB">2nd Client instance</param>
+        /// <returns>True if instances are not equal; otherwise, false</returns>
+        public static bool operator !=(ClientDTO clientA, ClientDTO clientB) => clientA.Firstname != clientB.Firstname || clientA.Lastname != clientB.Lastname || clientA.Passport != clientB.Passport;
+
+        /// <summary>
+        /// Defines equality of two instances of Client
+        /// </summary>
+        /// <param name="other">Given Client instance</param>
+        /// <returns>True if Client instance is equal to given; otherwise, false</returns>
+        public bool Equals(ClientDTO other)
+        {
+            return !(other is null) &&
+                   Firstname == other.Firstname &&
+                   Lastname == other.Lastname &&
+                   Passport == other.Passport &&
+                   Email == other.Email;
+        }
+
+        /// <summary>
+        /// Overriding of System.Object method Equals
+        /// </summary>
+        /// <param name="obj">Given object to compare with Client instance</param>
+        /// <returns>True if Client instance is equal to given object; otherwise, false</returns>        
+        public override bool Equals(object obj)
+        {
+            var client = obj as ClientDTO;
+            return this.Equals(client);
+        }
+
+        /// <summary>
+        /// Overriding of System.Object method GetHashCode
+        /// </summary>
+        /// <returns>Hashcode of Client instance</returns>
+        public override int GetHashCode()
+        {
+            var hashCode = 2056398814;
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(Firstname);
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(Lastname);
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(Passport);
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(Email);
+            return hashCode;
+        }
     }
 }
